180cc7f546c8e4fb16a033c0fa03436c89ee5803
Dien Vu <meotimdihia@gmail.com>
2016-03-20 00:55:32 +0700

    fix all yaml

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -3,6 +3,6 @@ kubectl create -f mysql-service.yaml
 kubectl create -f redis-controller.yaml
 kubectl create -f redis-service.yaml
 kubectl create -f php-fpm-controller.yaml
-kubectl create -f php-fpm-controller.yaml
+kubectl create -f php-fpm-service.yaml
 kubectl create -f nginx-controller.yaml
-kubectl create -f nginx-controller.yaml
+kubectl create -f nginx-service.yaml
