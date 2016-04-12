67f35a865dbc90f2619e4d1db6a8e7fe21a97881
Dien Vu <meotimdihia@gmail.com>
2016-04-12 22:57:56 +0700

    --

--- kubernetes/delete.sh
+++ kubernetes/delete.sh
@@ -1,2 +1,6 @@
 kubectl stop rc -l "name in (nginx, php-fpm, mysql, redis)"
 kubectl delete service -l "name in (nginx, php-fpm, mysql, redis)"
+
+kubectl delete -f disks-pod.yaml
+
+gcloud compute disks delete disk20 --zone=asia-east1-b
--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,3 +1,10 @@
+gcloud compute disks create disk20 --size=20GB --zone=asia-east1-b
+
+
+kubectl create -f volume-readonlymany.yaml
+kubectl create -f volume-claim-mysql.yaml
+kubectl create -f volume-claim-php-fpm.yaml
+
 kubectl create -f mysql-controller.yaml
 kubectl create -f mysql-service.yaml
 kubectl create -f redis-controller.yaml
@@ -6,3 +13,5 @@ kubectl create -f php-fpm-controller.yaml
 kubectl create -f php-fpm-service.yaml
 kubectl create -f nginx-controller.yaml
 kubectl create -f nginx-service.yaml
+
+kubectl create -f disks-pod.yaml
